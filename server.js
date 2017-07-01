8a7bc36300b608ef28ee363d12fffbeeb5c1f6cd
MathieuFan <mathieuquintois@gmail.com>
2017-07-01 19:33:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-let http = require('http')
-let fs = require ('fs')
-let server = http.createServer()
-
-
+let http     =  require('http')
+let fs       =  require('fs')
+let server   =  http.createServer()
+let url      =  require('url')
 server.on('request', (request, response) => {       
+var page = url.parse(request.url).pathname;
+    console.log('adresse utilisée: localhost:8080'+page);
+    
+    if(page == '/accueil'){ 
     fs.readFile('index.html', (err, data) => {
-        if (err) {
-            fs.readFile('err.html',(err, data) => {
+    response.writeHead(200)
+    response.end(data)
+        })
+      }else{
+    fs.readFile('err.html',(err, data) => {
                 response.writeHead(400)
                 response.end(data)
             })
-        }else{
-    response.writeHead(200)
-    response.end(data)
-        }
-    })    
-
-
+      }
 
 })
 server.listen(8080)
-console.log("Website on")
+console.log("Site web démarré")
